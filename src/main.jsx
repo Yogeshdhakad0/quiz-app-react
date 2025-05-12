1c2485e43daa5de011b67ea012349327682f9b82
yogeshdhakad0 <kalking121213@gmail.com>
2025-05-12 16:03:14 +0530

    ...

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { QuizProvider } from './Component/context/quizcontext.jsx'
 
 import { QuizProvider } from './Component/context/quizcontext.jsx';
 
