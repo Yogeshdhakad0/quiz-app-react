8d3f31c4c98feb33ec3840b6cdbcd90aa821b290
yogeshdhakad0 <kalking121213@gmail.com>
2025-05-12 15:49:30 +0530

    update name

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,10 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { QuizProvider } from './Component/context/quizcontext.jsx'
-import { QuizProvider } from "./components/context/quizcontext.jsx";
-import { QuizProvider } from "./Component/context/quizcontext.jsx";
-import { QuizProvider } from "./Component/context/quizcontext.jsx";
+// import { QuizProvider } from './Component/context/quizcontext.jsx'
+// import { QuizProvider } from "./components/context/quizcontext.jsx";
+// import { QuizProvider } from "./Component/context/quizcontext.jsx";
+// import { QuizProvider } from "./Component/context/quizcontext.jsx";
 
 
 createRoot(document.getElementById('root')).render(
