0629123b4b4b36149621248045e64732645ed8fa
yogeshdhakad0 <kalking121213@gmail.com>
2025-05-12 15:37:56 +0530

    new update

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { QuizProvider } from './Compontet/context/quizcontext.jsx'
+// import { QuizProvider } from './Compontet/context/quizcontext.jsx'
 
 createRoot(document.getElementById('root')).render(
 
