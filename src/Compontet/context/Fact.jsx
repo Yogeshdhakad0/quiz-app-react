791bbbcc447d6a67933877226ac1228b2738cbc6
yogeshdhakad0 <kalking121213@gmail.com>
2025-05-12 15:30:54 +0530

    update

--- src/Compontet/context/Fact.jsx
+++ src/Compontet/context/Fact.jsx
@@ -6,9 +6,6 @@ const  response = await fetch('https://opentdb.com/api.php?amount=10&category=18
 const  data= await  response.json()
 console.log(data)
 return data
-
-
-
 } catch (error) {
     console.log('errorr')
     
